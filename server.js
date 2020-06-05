16e36c9456de8026ad291a34c5adb16c66676b57
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-06-04 21:26:48 -0300

    Create subscribeToTimer function for socket.io

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const io = require('socket.io')();
 
 io.on('connection', (client) => {
+  client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date())
+    }, interval);
+  });
 });
 
 const port = 8000;
