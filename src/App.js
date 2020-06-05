d64e58a64c9de11f6585b2381c8adfcf54d466fc
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-06-04 22:21:42 -0300

    Use socket.io-client timestamp function in the UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { subscribeToTimer } from './api';
+
 
 function App() {
+  const [timestamp, setTimestamp] = useState('')
+  useEffect(() => {
+    subscribeToTimer((err, timestamp) => setTimestamp(timestamp));
+  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>socket.io-client</code>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>{timestamp}</span>
       </header>
     </div>
   );
